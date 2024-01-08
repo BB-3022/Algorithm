30ff75679053ccac95562c68118e0169e1e32c54
BB-3022 <139674894+BB-3022@users.noreply.github.com>
2024-01-08 19:25:29 +0900

    Update 오븐 시계.py

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -12,3 +12,13 @@ else :
     resultH = totalM//60
     resultM = totalM - (resultH * 60)
     print(resultH, resultM)
+
+# 코드 리팩토링
+H, M = map(int, input().split())
+U = int(input())
+
+totalM = (H * 60 + M + U) % 1440  # 총 소요 시간을 24시간(1440분)으로 나눈 나머지를 구함
+resultH = totalM // 60
+resultM = totalM % 60
+
+print(resultH, resultM)
