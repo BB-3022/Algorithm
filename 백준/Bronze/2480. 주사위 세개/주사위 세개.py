a3db138a4dca6b0d236ee2616cf2e12c169db308
BB-3022 <139674894+BB-3022@users.noreply.github.com>
2024-01-09 15:15:27 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,16 +1,15 @@
-num = list(map(int, input().split()))
+A, B, C = map(int, input().split())
 
 # 세 값이 동일 할 경우
-if num[0] == num[1] == num[2] :
-    print(10000+num[0]*1000)
+if A == B == C :
+    print(10000+A*1000)
 
-# 세 값이 모두 다를 경우
-elif num[0] != num[1] and num[1] != num[2] and num[0] != num[2] :
-    print(max(num) * 100)
+# 중복값이 있을 경우
+elif A == B or B == C or A ==C :
+    print(1000+A*100) if A == B else print(1000+C*100)
 
-# 중복 값이 있을 경우 (List Comprehension 사용)
+# 값이 전부 다를 경우
 else :
-    result = [value for value in num if num.count(value) == 2][0]
-    print(1000 + result * 100)
+    print(max(A,B,C)*100)
     
     
