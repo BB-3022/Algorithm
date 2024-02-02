a6f2ef6e4d510d1bbd0050a158b572634f895864
BB-3022 <139674894+BB-3022@users.noreply.github.com>
2024-02-02 14:51:01 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -2,12 +2,4 @@ count = int(input())
 numList = list(map(int, input().split()))
 findNum = int(input())
 
-resultCount = 0
-
-for i in range(count):
-    if numList[i]==findNum :
-        resultCount= resultCount + 1
-    else :
-        pass
-    
-print(resultCount)
+print(numList.count(findNum))
