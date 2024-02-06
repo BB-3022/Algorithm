8a908dfe9f19f2a544972893e66a8b7f0164eaa7
BB-3022 <139674894+BB-3022@users.noreply.github.com>
2024-02-06 23:20:39 +0900

    [Bronze III] Title: 최소, 최대, Time: 2524 ms, Memory: 321716 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,12 +9,11 @@ public class Main {
         int N = sc.nextInt();
         int[] arr = new int[N];
 
-        for(int i=0; i<N; i++){
+        for(int i=0; i<arr.length; i++){
             arr[i] = sc.nextInt();
         }
 
         Arrays.sort(arr);
-
         System.out.println(arr[0] + " " + arr[N-1]);
 
     }
